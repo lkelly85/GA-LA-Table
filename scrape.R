e677929f3c795f7ef672b2091963b94443cac31c
lkelly85 <41906906+lkelly85@users.noreply.github.com>
2022-01-03 14:24:42 +0000

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -9,9 +9,8 @@ library(magrittr)
 ##download data
 
 source <- "https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/e8454cf0-1152-4bcb-b9da-4343f625dfef/download/total_cases_by_la_20211231.csv"
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
 
-data <- read.csv(temp)
+data <- read.csv(source)
 
 data <- data %>%
   mutate(date=as.Date(as.character(Date), format="%Y%m%d")) %>%
