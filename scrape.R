d1c9353cd42ed02a579cfb838ba2973d1fe11421
lkelly85 <41906906+lkelly85@users.noreply.github.com>
2022-01-03 14:11:14 +0000

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -1,3 +1,11 @@
+##load libraries
+
+#load libraries
+library(curl)
+library(dplyr)
+library(magrittr)
+
+
 ##download data
 
 source <- "https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/e8454cf0-1152-4bcb-b9da-4343f625dfef/download/total_cases_by_la_20211231.csv"
